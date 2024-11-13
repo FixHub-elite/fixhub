73e09ebae47ed449a76fc87733e25f6cf9443e8c
Marcelinder <marcelbakgg@gmail.com>
2024-11-13 19:34:03 +0100

    all

---
+++ fixhub/src/main/java/com/vistula_elite/fixhub/FixhubApplication.java
@@ -0,0 +1,13 @@
+package com.vistula_elite.fixhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixhubApplication.class, args);
+	}
+
+}
---
+++ fixhub/src/test/java/com/vistula_elite/fixhub/FixhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vistula_elite.fixhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
